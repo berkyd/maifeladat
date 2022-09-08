3c6e1fc7d75269c837359eff373feaafc518bb28
Berky Dániel <info@petrik.hu>
2022-09-08 12:52:27 +0200

    feladat

--- feladat.js
+++ feladat.js
@@ -7,4 +7,19 @@ for(let i = 0;i<10;i++)
 tomb.sort();
 console.log(tomb)
 //2.feladat:
-let tomb2=[];
+let tomb2=[]
+for(let x = 0;x<20;x++)
+{
+    tomb[x] =
+    {
+        nev: 'Cirmos ${x}',
+        eletkor: Math.round(2+Math.random()*8)
+    }
+}
+for(let h = 0;h<tomb2.length;h++)
+{
+    if(tomb2[h].eletkor==2)
+    {
+        console.log(tomb2[h])
+    }
+}
